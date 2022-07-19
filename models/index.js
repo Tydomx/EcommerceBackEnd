bef56195ad962ccabd8ca605f78231efee9bafeb
Tydomx <michaelthai16@gmail.com>
2022-07-19 13:57:20 -0500

    Added small refactor to adjust

--- models/index.js
+++ models/index.js
@@ -17,20 +17,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'tags_on_product'
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  as: 'products_with_tag'
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
